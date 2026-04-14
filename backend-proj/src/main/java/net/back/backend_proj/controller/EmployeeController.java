7d69feb738a118b81c646d96781f83a110fe2386
mouad05 <mouad.kacimi05@gmail.com>
2026-04-14 10:42:25 +0100

    done

--- backend-proj/src/main/java/net/back/backend_proj/controller/EmployeeController.java
+++ backend-proj/src/main/java/net/back/backend_proj/controller/EmployeeController.java
@@ -15,16 +15,15 @@ import java.util.List;
 @RequestMapping("api/employees")
 public class EmployeeController {
     @Autowired
-    //build add employee rest api
+    // build add employee rest api
     private EmployeeService employeeService;
 
     @PostMapping("createEmployee")
-    public ResponseEntity<EmployeeDto>  createEmployee(@RequestBody EmployeeDto employeeDto) {
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
         EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
-        return  new ResponseEntity<>(savedEmployee , HttpStatus.CREATED);
+        // my name
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
-
-
 
     @GetMapping("id/{employeeId}")
     public ResponseEntity<EmployeeDto> getEmployeebyId(@PathVariable("employeeId") Long employeeId) {
@@ -34,25 +33,25 @@ public class EmployeeController {
 
     @GetMapping("allEmployees")
     public List<EmployeeDto> getAllEmployees() {
-        List<EmployeeDto> employees = employeeService.getAllEmployees() ;
-        return employees ;
+        List<EmployeeDto> employees = employeeService.getAllEmployees();
+        return employees;
     }
 
     @PutMapping("update/{id}")
-    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable Long id , @RequestBody EmployeeDto newEmployeeDto) {
-        EmployeeDto employeeDto1 =  employeeService.updateEmployee(id , newEmployeeDto) ;
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable Long id, @RequestBody EmployeeDto newEmployeeDto) {
+        EmployeeDto employeeDto1 = employeeService.updateEmployee(id, newEmployeeDto);
         return ResponseEntity.ok(employeeDto1);
     }
+
     @GetMapping("note/{id}")
-    public ResponseEntity<String> seeNote(@PathVariable Long id ) {
-        String Note = employeeService.seeNote(id) ;
+    public ResponseEntity<String> seeNote(@PathVariable Long id) {
+        String Note = employeeService.seeNote(id);
         return ResponseEntity.ok(Note);
     }
 
-
     @DeleteMapping("delete/{id}")
     public ResponseEntity<String> deleteEmployee(@PathVariable Long id) {
-        String employeeDto =  employeeService.deleteEmployee(id) ;
-        return ResponseEntity.ok("success") ;
+        String employeeDto = employeeService.deleteEmployee(id);
+        return ResponseEntity.ok("success");
     }
 }
